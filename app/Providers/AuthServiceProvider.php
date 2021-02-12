a0c044ba4f7364a738069ea45b4b2e407f5686f8
Alexandru <alexandru_panzaru@yahoo.com>
2021-02-12 22:18:10 +0200

    Message

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -27,8 +27,8 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Passport::routes();
-        
-        Passport::loadKeysFrom('/secret-keys/oauth');
+
+        Passport::loadKeysFrom('/storage');
         //
     }
 }
