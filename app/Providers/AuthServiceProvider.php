ea174ebe60fa68d3f67387dc1fe396313a040ece
Alexandru <alexandru_panzaru@yahoo.com>
2021-02-12 21:58:22 +0200

    Keys passport

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -27,7 +27,8 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Passport::routes();
-
+        
+        Passport::loadKeysFrom('/secret-keys/oauth');
         //
     }
 }
