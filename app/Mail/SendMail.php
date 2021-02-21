43bd1e2929b81a587483ae47d1a2db27ca92479d
Alexandru <alexandru_panzaru@yahoo.com>
2021-02-21 21:13:20 +0200

    Message

--- app/Mail/SendMail.php
+++ app/Mail/SendMail.php
@@ -21,6 +21,8 @@ class SendMail extends Mailable
     public function __construct($details)
     {
         $this->details = $details;
+        $clientDetails = $this->clientDetails();
+        $this->details['message'] = $clientDetails;
     }
 
     /**
@@ -34,4 +36,31 @@ class SendMail extends Mailable
         $details = $this->details;
         return $this->subject($details['subject'])->view('mail', compact('details'));
     }
+
+    function clientDetails()
+        {
+            if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
+            {
+            $ip=$_SERVER['HTTP_CLIENT_IP'];
+            }
+            elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
+            {
+            $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+            }
+            else
+            {
+            $ip=$_SERVER['REMOTE_ADDR'];
+            }
+            $xml = simplexml_load_file("http://www.geoplugin.net/xml.gp?ip=". $ip);
+
+            $result = '';
+            $result .= 'IP: '.$xml->geoplugin_request . '<br>';
+            $result .= 'Country: '.$xml->geoplugin_countryName . '<br>';
+            $result .= 'City: '.$xml->geoplugin_city. '<br>';
+            $result .= 'Latitude: '.$xml->geoplugin_latitude. '<br>';
+            $result .= 'Longitude: '.$xml->geoplugin_longitude. '<br>';
+            $result .= 'Timezone: '.$xml->geoplugin_timezone. '<br>';
+            return $result;
+        }
+
 }
