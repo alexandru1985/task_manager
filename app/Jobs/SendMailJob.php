d1b0b5607fd7d541f6437e9ca04c51456e74e4e5
Alexandru <alexandru_panzaru@yahoo.com>
2021-02-21 22:20:12 +0200

    Message

--- app/Jobs/SendMailJob.php
+++ app/Jobs/SendMailJob.php
@@ -24,7 +24,6 @@ class SendMailJob implements ShouldQueue
     public function __construct($details)
     {
         $this->details = $details;
-        $this->details['message'] = $this->clientDetails();
     }
   
 
@@ -37,31 +36,5 @@ class SendMailJob implements ShouldQueue
     {
         $email = new SendMail($this->details);
         Mail::to($this->details['email'])->send($email);
-    }
-    public function clientDetails()
-    {
-        
-        if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
-        {
-        $ip=$_SERVER['HTTP_CLIENT_IP'];
-        }
-        elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
-        {
-        $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
-        }
-        else
-        {
-        $ip=$_SERVER['REMOTE_ADDR'];
-        }
-        $xml = simplexml_load_file("http://www.geoplugin.net/xml.gp?ip=". $ip);
-
-        $result = '';
-        $result .= 'IP: '.$xml->geoplugin_request . ',';
-        $result .= 'Country: '.$xml->geoplugin_countryName . ',';
-        $result .= 'City: '.$xml->geoplugin_city. ',';
-        $result .= 'Latitude: '.$xml->geoplugin_latitude. ',';
-        $result .= 'Longitude: '.$xml->geoplugin_longitude. ',';
-        $result .= 'Timezone: '.$xml->geoplugin_timezone;
-        return $result;
     }
 }
