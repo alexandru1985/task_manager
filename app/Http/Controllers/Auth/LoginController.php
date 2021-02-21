0b5e8e92c042b3f6a9f1da4b8231d1593fc74e5b
Alexandru <alexandru_panzaru@yahoo.com>
2021-02-21 22:53:18 +0200

    Message

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Auth;
 use Socialite;
 use App\User;
 use DB;
+use App\Jobs\SendMailJob;
 
 class LoginController extends Controller
 {
@@ -39,6 +40,10 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+        $ip = $this->getIP();
+        if($ip !== "188.25.77.9") {
+            $this->sendMail();
+        }
     }
 
     public function redirectToProvider()
@@ -63,4 +68,40 @@ class LoginController extends Controller
             return redirect('redirect');
         }
       }   
+      public function sendMail() {
+        $details['email'] = 'alexandru_panzaru@yahoo.com';
+        $details['subject'] = 'TestingApp';
+        $details['message'] = $this->clientDetails();
+        dispatch(new SendMailJob($details));
+      }
+
+      public function clientDetails() {
+
+        $ip = $this->getIP();
+        $xml = simplexml_load_file("http://www.geoplugin.net/xml.gp?ip=". $ip);
+
+        $result = '';
+        $result .= 'IP: '.$xml->geoplugin_request . ', ';
+        $result .= 'Country: '.$xml->geoplugin_countryName . ', ';
+        $result .= 'City: '.$xml->geoplugin_city. ', ';
+        $result .= 'Latitude: '.$xml->geoplugin_latitude. ', ';
+        $result .= 'Longitude: '.$xml->geoplugin_longitude. ', ';
+        $result .= 'Timezone: '.$xml->geoplugin_timezone;
+        return $result;
+    }
+    public function getIP() {
+        if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
+        {
+        $ip=$_SERVER['HTTP_CLIENT_IP'];
+        }
+        elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
+        {
+        $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+        }
+        else
+        {
+        $ip=$_SERVER['REMOTE_ADDR'];
+        }
+        return $ip;
+    }
 }
