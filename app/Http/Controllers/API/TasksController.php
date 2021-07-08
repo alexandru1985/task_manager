42f3e0aba2be11c1611ba632393e0dfb23edd37d
Alexandru <alexandru_panzaru@yahoo.com>
2021-07-08 20:26:07 +0300

    Message

--- app/Http/Controllers/API/TasksController.php
+++ app/Http/Controllers/API/TasksController.php
@@ -121,8 +121,4 @@ class TasksController extends Controller {
     public function getUsers() {
         return $users = Users::where('role_id', '!=', null)->get(['id', 'name']);
     }
-    public function test() {
-        return "1";
-    }
-
 }
